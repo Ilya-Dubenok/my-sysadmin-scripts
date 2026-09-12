51403a38ce7b309549dfa2ecc74e2607df87d8d2
Ilya Dubenok <dubenokilya@gmail.com>
2026-09-12 14:54:46 +0300

    Добавил дату и отступы по тексту

--- script.sh
+++ script.sh
@@ -2,11 +2,18 @@
 INTERVAL=1
 LOGFILE="monitor.log"
 
-while true; do
-    echo "current date-time" >> "$LOGFILE"
-    free -h >> "$LOGFILE"
-    df -h >> "$LOGFILE"
-    uptime >> "$LOGFILE"
+new_line() {
     echo "" >> "$LOGFILE"
+}
+
+while true; do
+    echo "---$(date '+%Y-%m-%d %H:%M:%S')---" >> "$LOGFILE"
+    new_line
+    free -h >> "$LOGFILE"
+    new_line
+    df -h >> "$LOGFILE"
+    new_line
+    uptime >> "$LOGFILE"
+    new_line
     sleep "$INTERVAL"
 done
