fc82a8f3eb7822474273672896921c6d57412169
Ilya Dubenok <dubenokilya@gmail.com>
2026-09-12 15:30:22 +0300

    Работа в фоне, обработка ошибок, пример вывода

--- script.sh
+++ script.sh
@@ -1,10 +1,20 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 INTERVAL=1
 LOGFILE="monitor.log"
 
 new_line() {
     echo "" >> "$LOGFILE"
 }
+
+if [ "$1" = "-b" ]; then
+    nohup "$0" >/dev/null 2>&1 &
+    disown
+    echo "Run in background, PID: $!"
+    exit 0
+elif [ -n "$1" ]; then
+    echo "error: unexpected argument '$1'" >&2
+    exit 1
+fi
 
 while true; do
     echo "---$(date '+%Y-%m-%d %H:%M:%S')---" >> "$LOGFILE"
